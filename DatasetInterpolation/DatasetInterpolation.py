e6b18fb22fe1403363cda229ad6dd6b91a27cf36
Tristan Pressley <trspres@gmail.com>
2016-12-11 01:56:02 -0500

    Implemented subdivide method Implemented a subdivision method so we can get the values between the given datapoints Additionally fixed some bugs in the interpolation function

--- DatasetInterpolation/DatasetInterpolation.py
+++ DatasetInterpolation/DatasetInterpolation.py
@@ -4,15 +4,30 @@ def main():
     xs = [0,1,2,3,4]
     ys = [1,2,4,8,16]
 
+    ls = subdivide(xs,7)
+    for x in ls:
+        evalLagrangeInterpolation(x,xs,ys)
 
 def evalLagrangeInterpolation(x, xs, ys):
     r = 0
     
-    for j in range(0,len(y)):
+    for j in range(0,len(ys)):
         m = 1
-        for k in range(len(y)):
-            if (xs[k] != xs[j]):
+        for k in range(len(xs)):
+            if (k != j):
                 m *= (x - xs[k]) / (xs[j] - xs[k])
-            r += ys[j] * m
+        r += ys[j] * m
+    print (r)
     return r
+
+def subdivide(xs,count):
+    ts = xs.copy()
+    rs = xs.copy()
+    while count > 0:
+        count -= 1
+        for i in range(0,len(ts)-1):
+            rs.insert(i+1+i,(ts[i]+ts[i+1])/2)
+        ts = rs.copy()
+    return rs
+
 main()
